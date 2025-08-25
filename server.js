e6d9518e23315c396cd1c164c286c0c05f794415
anuj taywade <anujtaywade456@gmail.com>
2025-08-25 13:13:18 +0530

    final commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URL,{
+// require("dotenv").config();
+// console.log("MONGO_URL from env:", process.env.MONGO_URL);
 
-})
-.then(()=>console.log("conected to DB"))
-.catch((err)=>console.log("error in DB connection",err))
-console.log("ENV MONGO_URL =", process.env.MONGO_URL);
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.MONGO_URL,{
+
+// })
+// .then(()=>console.log("conected to DB"))
+// .catch((err)=>console.log("error in DB connection",err))
+// console.log("ENV MONGO_URL =", process.env.MONGO_URL);
 
 
 const express = require('express')
